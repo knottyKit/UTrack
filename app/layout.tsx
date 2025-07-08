39768cd0bdba0f63ce8dab64ee59becc96e30ee2
knottykit <kit.collene@gmail.com>
2025-07-08 12:47:32 +0800

    build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,7 @@ import { Analytics } from "@vercel/analytics/next";
 import ConditionalBottomNav from "@/components/ConditionalBottomNav";
 import { ThemeProvider } from "next-themes";
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
-  display: "swap",
-});
+
 
 export const metadata: Metadata = {
   title: "Taguru",
@@ -28,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${poppins.variable} antialiased`}
+      className={` antialiased`}
       suppressHydrationWarning
     >
       <body className="w-full overflow-auto h-screen pb-16">
